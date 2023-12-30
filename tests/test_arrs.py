2677bcb9645a1443ba633d8c5a0ec8edc43a24db
Alexandr Sinev <sir.sineow@yandex.ru>
2023-12-31 00:16:57 +0300

    add dicts.py and add new tests for dicts.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -14,9 +14,8 @@ def test_slice():
 
 
 def test_get_val():
-    def test_get_val():
-        assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
-        assert dicts.get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
-        assert dicts.get_val({}, "vcs", "git") == "git"
-        assert dicts.get_val({}, "vcs", "bazaar") == "bazaar"
+    assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
+    assert dicts.get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+    assert dicts.get_val({}, "vcs", "git") == "git"
+    assert dicts.get_val({}, "vcs", "bazaar") == "bazaar"
 
