6b70b42f980a9871089a2954ff196eb5b3c41eb9
Alexandr Sinev <sir.sineow@yandex.ru>
2023-12-31 00:01:24 +0300

    add dicts.py and add new tests for dicts.py

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,11 +1,22 @@
-from utils import arrs
+from utils import arrs, dicts
 
 
 def test_get():
-    assert arrs.get([1, 2, 3], 1, "test") == 3
+    assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1, 2, 3]) == [1, 2, 3]
+
+
+def test_get_val():
+    def test_get_val():
+        assert dicts.get_val({"vcs": "mercurial"}, "vcs") == "mercurial"
+        assert dicts.get_val({"vcs": "mercurial"}, "vcs", "git") == "mercurial"
+        assert dicts.get_val({}, "vcs", "git") == "git"
+        assert dicts.get_val({}, "vcs", "bazaar") == "bazaar"
+
